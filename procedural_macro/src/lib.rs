f1f4df0adb7fb2a11008a00b4ae139db12e5323f
Aliénore Bouttefeux <alienore.bouttefeux@gmail.com>
2021-02-24 13:39:43 +0100

    procedural macro uwing quote

--- procedural_macro/src/lib.rs
+++ procedural_macro/src/lib.rs
@@ -1,23 +1,50 @@
 
 extern crate proc_macro;
 extern crate quote;
+extern crate syn;
+extern crate proc_macro2;
 
+use quote::quote;
 use proc_macro::TokenStream;
 
 #[proc_macro]
 pub fn implement_direction_list(_item: TokenStream) -> TokenStream {
-    let mut string = String::default();
-    for i in 1..=127 {
-        let mut array_direction = String::default();
-        let mut array_direction_positives = String::default();
+    let mut implem = vec![];
+    for i in 1_usize..=127_usize {
+        let mut array_direction = vec![];
+        let mut array_direction_positives = vec![];
         for j in 0..i {
-            array_direction.push_str(&format!("Direction{{index_dir: {}, is_positive: {}, _phantom: PhantomData}},", j, true));
-            array_direction.push_str(&format!("Direction{{index_dir: {}, is_positive: {}, _phantom: PhantomData}},", j, false));
-            array_direction_positives.push_str(&format!("Direction{{index_dir: {}, is_positive: {}, _phantom: PhantomData}},", j, true));
+            array_direction.push(
+                quote!{
+                    Direction{index_dir: #j, is_positive: true, _phantom: PhantomData},
+                    Direction{index_dir: #j, is_positive: false, _phantom: PhantomData}
+                }
+            );
+            array_direction_positives.push(
+                quote!{
+                    Direction{index_dir: #j, is_positive: true, _phantom: PhantomData}
+                }
+            );
         }
-        
-        string.push_str(&format!("const U{i}_DIR: [Direction<U{i}>; {i} * 2] = [{array_dir}]; const U{i}_DIR_POS: [Direction<U{i}>; {i}] = [{array_positive}]; impl DirectionList for Direction<U{i}> {{ fn get_all_directions() -> & 'static [Self] {{ &U{i}_DIR }} fn get_positives_directions() -> & 'static [Self] {{ &U{i}_DIR_POS }} }}",
-        i = i, array_dir = array_direction, array_positive = array_direction_positives));
+        let u_indent = syn::Ident::new(&format!("U{}", i), proc_macro2::Span::call_site());
+        let u_dir_indent = syn::Ident::new(&format!("U{}_DIR", i), proc_macro2::Span::call_site());
+        let u_dir_pos_indent = syn::Ident::new(&format!("U{}_DIR_POS", i), proc_macro2::Span::call_site());
+        let s = quote!{
+            const #u_dir_indent: [Direction<#u_indent>; #i * 2] = [ #(#array_direction),* ];
+            const #u_dir_pos_indent: [Direction<#u_indent>; #i] = [ #(#array_direction_positives),* ];
+            impl DirectionList for Direction<#u_indent> {
+                fn get_all_directions() -> & 'static [Self] {
+                    &#u_dir_indent
+                }
+                fn get_positives_directions() -> & 'static [Self] {
+                    &#u_dir_pos_indent
+                }
+            }
+        };
+        implem.push(s);
     }
-    string.parse().unwrap()
+    let final_stream = quote!{
+        #(#implem)*
+    };
+    final_stream.into()
 }
